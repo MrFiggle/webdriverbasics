9de37ae67f806e0b7a8ba0e43d95c0b45a78669b
M <ppor143@Peters-MacBook-Pro.local>
2016-01-24 18:50:37 -0800

    Finished Interrogation exercises

---
+++ src/test/java/com/seleniumsimplified/junit/DriverLevelInterrogationExercise.java
@@ -0,0 +1,7 @@
+package com.seleniumsimplified.junit;
+
+/**
+ * Created by ppor143 on 1/22/16.
+ */
+public class DriverLevelInterrogationExercise {
+}
---
+++ src/test/java/com/seleniumsimplified/junit/FindElementsExercise.java
@@ -0,0 +1,7 @@
+package com.seleniumsimplified.junit;
+
+/**
+ * Created by ppor143 on 1/23/16.
+ */
+public class FindElementsExercise {
+}
